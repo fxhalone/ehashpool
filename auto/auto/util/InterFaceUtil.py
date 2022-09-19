418f272ec9155a436e1b1799e3e4c722adb620dd
fxhalone <f1035717636@163.com>
2022-09-19 12:16:34 +0800

    sdsad

--- auto/auto/util/InterFaceUtil.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-class InterFaceUtil:
-    headers = {
-        # 'Authorization': requestHeader,
-        'Content-Type': 'application/json'
-    }
--- auto/auto/util/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-headers = {
-    'accept': 'application/json',
-    'Content-Type': 'application/json'
-}
-response = requests.get(url='http://ehashapi.test.poolx.io/pool/v1/miner/Home', headers=headers)
-
-assert int(response.json()['code']) == 200
